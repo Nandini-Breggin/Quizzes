985ce6c8ddf8509c6490521cc17bdf11b74069d7
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-25 20:00:31 -0600

    Updated the code

--- Admissions.java
+++ Admissions.java
@@ -5,73 +5,102 @@ public class Admissions {
 	public static void main( String [] args ) {
 		
         Scanner input = new Scanner( System.in );
+
         
-        System.out.print( "Enter your first name: " );
-        String first_name = input.nextLine();
 
-        System.out.print( "Enter your last name: " );
-        String last = input.nextLine();
+        System.out.print( "Enter your last name:  \n" );
+        String last_name = input.nextLine();
 
-        System.out.print( "Enter your SAT Score (0-1600): " );
+        int s = 0;
+
+
+        if ("Padjen".equals(last_name)) {
+            System.out.print( "You got in! Reason: Your last name is Padjen \n" );
+            System.exit(0);
+        } else if  ("padjen".equals(last_name)) {
+            System.out.print( "You got in! Reason: Your last name is Padjen \n" );
+            System.exit(0);
+        } else {
+            System.out.print("Nope! Please continue on: \n" );
+        }
+
+        System.out.print( "Do you live in Colorado? (yes, no: Florida, other)  \n" );
+        String residence_1 = input.nextLine(); 
+    
+        if ("yes".equals(residence_1)) { 
+            System.out.print("Elite! You got in!\n Reason: You got an elite in one metric!\n");
+            System.exit(0);
+        } else if ("no".equals(residence_1)) {
+            System.out.print("Nope! You didn't get in. Reason: You got lower than Acceptable in a metric. \n");
+            System.exit(0);
+        } else if ("other".equals(residence_1)) {
+            System.out.print("Acceptable \n");
+        }
+        
+
+        System.out.print( "Enter your SAT Score (0-1600): \n" );
         int SAT = input.nextInt();
-
-        System.out.print( "Enter your Class Rank Percentile: " );
-        double rank = input.nextDouble();
-        
-		System.out.print( "Enter your Awards Earned (0-10+): " );
-        int award = input.nextInt();
-        
-		System.out.print( "Where is your residence? (state): " );
-        String residence = input.nextLine();
-        
-
-        //String SAT_rating = 0;
-        //String class_rank = 0;
-        //String award_rating = 0;
-        //String residence_rating = 0;
-        
-        
         
 		if (SAT >= 1500) { 
-            System.out.print("Elite");
+            System.out.print("Elite! You got in!\n Reason: You got an elite in one metric!\n");
+            System.exit(0);
         } else if (SAT >= 1400) {
-            System.out.print("Solid");
+            System.out.print("Solid \n");
+            s = s + 1;
         } else if (SAT >= 1200) {
-            System.out.print("Acceptable");
+            System.out.print("Acceptable \n");
         } else {
-            System.out.print("Nope");
+            System.out.print("Nope! You didn't get in. Reason: You got lower than Acceptable in a metric. \n");
+            System.exit(0);
         }
+
+
+        System.out.print( "Enter your Class Rank Percentile: \n" );
+        double rank = input.nextDouble();
 
         if (rank >= 95) { 
-			System.out.print("Elite");
+			System.out.print("Elite! You got in!\n Reason: You got an elite in one metric!\n");
+            System.exit(0);
         } else if (rank >= 90) {
-            System.out.print("Solid");
+            System.out.print("Solid \n");
+            s = s + 1;
         } else if (rank >= 85) {
-            System.out.print("Acceptable");
+            System.out.print("Acceptable \n");
         } else {
-            System.out.print("Nope");
+            System.out.print("Nope! You didn't get in. Reason: You got lower than Acceptable in a metric. \n");
+            System.exit(0);
         }
+
+
+        System.out.print( "Enter your Awards Earned (0-10+): \n" );
+        int award = input.nextInt();
 
         if (award >= 10) { 
-			System.out.print("Elite");
+			System.out.print("Elite! You got in!\n Reason: You got an elite in one metric!\n");
+            System.exit(0);
         } else if (award >= 8) {
-            System.out.print("Solid");
+            System.out.print("Solid \n");
+            s = s + 1;
         } else if (award >= 3) {
-            System.out.print("Acceptable");
+            System.out.print("Acceptable \n");
         } else {
-            System.out.print("Nope");
+            System.out.print("Nope! You didn't get in. Reason: You got lower than Acceptable in a metric. \n");
+            System.exit(0);
+        } 
+
+        // System.out.print( "\n" );
+        
+        if (s >= 2) {
+            System.out.print("Solid! You got in!\n Reason: You got solid for 2 metrics!\n");
+            System.exit(0);
         }
 
-        if (residence = "Colorado") { 
-			System.out.print("Elite");
-        } else if (residence = "Florida") {
-            System.out.print("Nope");
-        } else {
-            System.out.print("Acceptable");
-        }
+        System.out.print( "Testing \n");
+
+
         
-        
-        
+
+
 
     }
     
